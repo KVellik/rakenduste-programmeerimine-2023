d7e55bccadfe142704a4f129d9f4309440c0f576
Karl <wellik@tlu.ee>
2023-09-13 12:49:50 +0300

    Teise tunni check

---
+++ lessons/13-09-2023/basic.js
@@ -0,0 +1,17 @@
+// Luua array numbritega ja leida spetsiifiline numbri index
+//console.log("KMV")
+
+const array = [1, 2, 3, 4, 5, 6, "hi"]
+
+function findMyIndex(array, element){
+    return array.indexOf(element)
+}
+
+console.log(findMyIndex(array, 4))
+
+
+function sum(num1, num2){
+    
+}
+
+console.log(sum)
